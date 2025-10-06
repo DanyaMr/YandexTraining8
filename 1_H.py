bb5ca7e0f60f2521cbefc421d4e95e0789f5bcac
Даниил Морозов <dan4ik082005@mail.ru>
2025-10-06 15:45:18 +0300

    bebra

---
+++ 1_H.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def main():
+
+    n, m = map(int, sys.stdin.readline().strip().split())
+
+    arr = list()
+
+    source = sys.stdin.readline().strip()
+
+    for _ in range(n):
+        s = list(sys.stdin.readline().strip())
+        arr.append(s)
+
+    
+
+                 
+
+if __name__ == '__main__':
+    main()
